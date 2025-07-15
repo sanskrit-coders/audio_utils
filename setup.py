430b21aabb29e10bf7ae8fa3c5608d256be0d7ed
vvasuki <vishvas.vasuki@gmail.com>
2025-07-15 18:28:43 +0530

    sd

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
         'Topic :: Text Processing :: Linguistic',
 
         # Pick your license as you wish (should match "license" above)
-        'License :: OSI Approved :: MIT License',
+        # 'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
